1533e5daaf539b6f5244b1776306929d3d6e70be
Aar0n <aaroms9733@gmail.com>
2012-07-26 17:22:06 +0800

    Use the extractor proxy

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	 chrome.tabs.executeScript(null, { file: "lib/jquery.min.js" }, function() {
-	        chrome.tabs.executeScript(null, { file: "lib/readability.js" });
-	 });
-});
